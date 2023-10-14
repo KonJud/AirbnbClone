a1ae11be02b30345598dd73aa1f491a30c9a2848
KonJud <deveuudes@gmail.com>
2023-10-14 18:48:26 +0000

    commit init

---
+++ airbnb-clone/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+
+const font = Nunito({
+  subsets: ['latin']
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
